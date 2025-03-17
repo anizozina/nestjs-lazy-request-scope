1167a63c1db9deabcfea8aaa860af63c12d6bb16
anizozina <anizozina@gmail.com>
2025-03-18 06:53:43 +0900

    chore: remove unnecessary codes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,16 +5,10 @@ import { LazyModuleLoader } from '@nestjs/core';
 export class AppController {
   constructor(private lazyModuleLoader: LazyModuleLoader) {}
 
-  private lastModuleRef: any = undefined;
-
   @Get('not-work')
   async notWork() {
     const { LazyModule } = await import('./lazy/module');
     const moduleRef = await this.lazyModuleLoader.load(() => LazyModule);
-    // const moduleRef = await this.lazyLoadModule.load(() => LazyModule.unique()); // works
-
-    if (!this.lastModuleRef) this.lastModuleRef = moduleRef;
-    else console.log(this.lastModuleRef === moduleRef);
 
     const { ServiceOne } = await import('./lazy/one.service');
     const _service = await moduleRef.resolve(ServiceOne);
--- src/lazy/module.ts
+++ src/lazy/module.ts
@@ -1,21 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ServiceOne } from './one.service';
+import { ServiceThree } from './three.service';
 import { ServiceTwo } from './two.service';
 
 @Module({
-  providers: [ServiceOne, ServiceTwo],
+  providers: [ServiceOne, ServiceTwo, ServiceThree],
   exports: [ServiceOne],
 })
 export class LazyModule {
   static unique() {
     return {
-      module: LazyModule,
-      providers: [
-        {
-          provide: 'noop',
-          useValue: Math.random(), // ensure that always a new instance will be created
-        },
-      ],
+      module: LazyModule
     };
   }
 }
